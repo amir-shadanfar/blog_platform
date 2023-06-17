daa872389301541dbd4e6d7069ff79d003a5c2ac
amir <amir.shadanfar@narbulut.com.tr>
2023-06-17 23:33:47 +0300

    define middleware & fix routes

---
+++ src/config/middlewares.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use App\MiddlewareDispatcher;
+use App\Middlewares\StartSessionMiddleware;
+use App\Middlewares\ValidationErrorsMiddleware;
+use App\Middlewares\ValidationExceptionMiddleware;
+
+return function (MiddlewareDispatcher $middlewareDispatcher) {
+    $middlewareDispatcher->addMiddleware(StartSessionMiddleware::class);
+    $middlewareDispatcher->addMiddleware(ValidationErrorsMiddleware::class);
+    $middlewareDispatcher->addMiddleware(ValidationExceptionMiddleware::class);
+};
--- src/config/routes.php
+++ src/config/routes.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Middlewares\AuthMiddleware;
+use App\Middlewares\GuestMiddleware;
 use App\View;
 use App\Router;
 use App\Controllers\AuthController;
@@ -12,17 +14,17 @@ return function (Router $router) {
     // blog
     $router->get('/', [BlogController::class, 'index']);
     $router->get('/blog', [BlogController::class, 'show']);
-    $router->get('/blog/create', [BlogController::class, 'create'])->only('auth');
+    $router->get('/blog/create', [BlogController::class, 'create'])->middleware(AuthMiddleware::class);
     $router->post('/blog', [BlogController::class, 'store']);
     // auth
-    $router->get('/login', [AuthController::class, 'loginPage'])->only('guest');
+    $router->get('/login', [AuthController::class, 'loginPage'])->middleware(GuestMiddleware::class);
     $router->post('/login', [AuthController::class, 'login']);
-    $router->get('/register', [AuthController::class, 'registerPage'])->only('guest');
+    $router->get('/register', [AuthController::class, 'registerPage'])->middleware(GuestMiddleware::class);
     $router->post('/register', [AuthController::class, 'register']);
-    $router->post('/logout', [AuthController::class, 'logout'])->only('auth');
+    $router->post('/logout', [AuthController::class, 'logout'])->middleware(AuthMiddleware::class);
     // comment
-    $router->post('/comment', [CommentController::class, 'store'])->only('auth');
-    $router->delete('/comment', [BlogController::class, 'delete'])->only('auth');
+    $router->post('/comment', [CommentController::class, 'store'])->middleware(AuthMiddleware::class);
+    $router->delete('/comment', [BlogController::class, 'delete'])->middleware(AuthMiddleware::class);
 
     // closure routeing without rendering with twig
     $router->get('/imprint', function () {
